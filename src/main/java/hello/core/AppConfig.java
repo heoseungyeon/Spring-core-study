5dd70dea6b669c01cb29f8777f277df54a70110a
heoseungyeon <dia0312@naver.com>
2021-02-15 20:25:53 +0900

    DI through AppConfig Constructor Injection

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,19 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderSerivce;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+
+    public MemberService memberService(){
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderSerivce orderService(){
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+}
