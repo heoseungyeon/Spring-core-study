bfa5611ef4873a45471ead017c5ff8ccbbec05f9
heoseungyeon <dia0312@naver.com>
2021-02-23 13:03:39 +0900

    @componentScan study

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.core.member",
+//        basePackageClasses = AutoAppConfig.class, //해당 클래스 패키지가 베이스가 됨.
+//        지정하지 않으면 @ComponentScan 이 붙은 설정 클래스의 패키지가 베이스 패키지가 됨.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //기존의 AppConfig가 컴포넌트스캔 되지 않도록 필터링함.
 )
 public class AutoAppConfig {
